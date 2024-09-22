d51a4c8b362a06d21d36419fd69f72488ba379a6
K.Deepa shree <121483955+Deepashree8@users.noreply.github.com>
2024-09-22 04:49:21 +0000

    models file

--- carbon_footprint_tracker/tracker/models.py
+++ carbon_footprint_tracker/tracker/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class PurchaseCategory(models.Model):
+    name = models.CharField(max_length=100)
+    carbon_footprint_per_unit = models.FloatField()  # kg CO2 per unit
+
+    def __str__(self):
+        return self.name
+
+class Purchase(models.Model):
+    category = models.ForeignKey(PurchaseCategory, on_delete=models.CASCADE)
+    amount = models.FloatField()  # Quantity of the item
+    date = models.DateField(auto_now_add=True)
+
+    def carbon_footprint(self):
+        return self.amount * self.category.carbon_footprint_per_unit
