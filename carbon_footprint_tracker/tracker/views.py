1dfb307e920969256a078a0a0b798dffeedd7070
K.Deepa shree <121483955+Deepashree8@users.noreply.github.com>
2024-09-22 05:02:42 +0000

    view file

--- carbon_footprint_tracker/tracker/views.py
+++ carbon_footprint_tracker/tracker/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Purchase, PurchaseCategory
+
+def home(request):
+    if request.method == 'POST':
+        category_id = request.POST.get('category')
+        amount = float(request.POST.get('amount'))
+        category = PurchaseCategory.objects.get(id=category_id)
+        purchase = Purchase.objects.create(category=category, amount=amount)
+        carbon_footprint = purchase.carbon_footprint()
+        return render(request, 'tracker/result.html', {'carbon_footprint': carbon_footprint})
+
+    categories = PurchaseCategory.objects.all()
+    return render(request, 'tracker/home.html', {'categories': categories})
+
